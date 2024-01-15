a4c95ecf31480fd85986f84cb887d80d6b7cc457
PoMaksBo <1994Deterok@mail.ru>
2024-01-15 21:08:23 +0500

    testing task

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ClientService} from '../../services/client.service';
+import { ClientRecord } from 'src/app/interfaces/interfaces';
+import { forkJoin, map, mergeMap, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -9,9 +11,30 @@ import {ClientService} from '../../services/client.service';
 })
 export class ListComponent implements OnInit {
 
+  public clientData?: ClientRecord[];
+
   constructor(private readonly clientService: ClientService) { }
 
   ngOnInit(): void {
+    this.clientService.list()
+    .pipe(
+      switchMap(val => {
+        return forkJoin(
+          of(val),
+          this.clientService.getAccounts(val.map(el => el.id)),
+          forkJoin(...val.map(el => this.clientService.getContact(el.id))),
+        )
+      })
+    )
+    .subscribe(([nameList, currencyList, infoList]) => {
+        this.clientData = infoList.map(el => ({
+          ...el,
+          id: el.userId,
+          name: nameList.find(nameEl => nameEl.id === el.userId)?.name ?? '',
+          currency: currencyList.find(nameEl => nameEl.userId === el.userId)?.currency ?? '',
+        }));
+    });
   }
+
 
 }
